07b03305e0b86212f3f82b72f2f3678de7852e04
Akula404 <berrycarloschernobyl@gmail.com>
2026-02-15 14:50:12 +0300

    trial3

--- jobcard/views.py
+++ jobcard/views.py
@@ -34,6 +34,7 @@ def temp_submission(request):
     # ---------- PAGE LOAD ----------
     lines = ["line1","line2","line3","line4","line5","line6","line7"]
     forms_data = []
+
     for line in lines:
         obj, _ = TempSubmission.objects.get_or_create(
             operator=user,
