16694daac63edfd8933e7c5d865dc1efc31f5f0d
Akula404 <berrycarloschernobyl@gmail.com>
2026-03-05 16:56:35 +0300

    Update models.py

--- jobcard/models.py
+++ jobcard/models.py
@@ -88,8 +88,8 @@ class JobCard(models.Model):
         ordering = ["-date", "line"]
         constraints = [
             models.UniqueConstraint(
-                fields=["date", "line", "shift"],
-                name="unique_jobcard_per_shift_line_date"
+                fields=["date", "line", "shift", "wo_number"],
+                name="unique_jobcard_per_workorder"
             )
         ]
         indexes = [
@@ -138,7 +138,7 @@ class TempSubmission(models.Model):
 
         constraints = [
             models.UniqueConstraint(
-                fields=["operator", "date", "line", "shift", "wo_number"],
+                fields=["operator", "date", "line", "shift"],
                 name="unique_operator_submission"
             )
         ]
